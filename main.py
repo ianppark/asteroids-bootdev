3605dc5e23f8f6515998d037add01c42e481f474
ianppark <ianppark@gmail.com>
2024-12-28 12:38:04 -0500

    Add player class and draw player to center of screen

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pygame
 from pygame import Color
 from constants import *
+from player import Player
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    # Initialize pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+    # Main Game Loop
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+
         screen.fill(Color(0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
